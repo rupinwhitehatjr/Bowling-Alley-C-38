228c5ee818c2140faf5b68e691e884e23e677198
rupinwhitehatjr <61224617+rupinwhitehatjr@users.noreply.github.com>
2020-07-08 21:04:50 +0530

    Add files via upload

--- js/Ball.js
+++ js/Ball.js
@@ -1,12 +1,15 @@
 class Ball{
     constructor(){
-
+        this.xPos=0;
+        this.yPos=0;
+        this.ballSprite=createSprite(150,600);
+        this.ballSprite.addAnimation("redBall",redBall);
     }
    
     display(){
-        ball1=createSprite(150,600);
-        ball1.addAnimation("redBall",redBall);
-        ball1.addAnimation("redRunning",redBallImg);
+        
+        this.ballSprite.addAnimation("redBall",redBall);
+        /*ball1.addAnimation("redRunning",redBallImg);
         ball1.scale=0.03;
         ball1.setCollider("circle",0,0,1000);
 
@@ -14,6 +17,16 @@ class Ball{
         ball2.addAnimation("blueBall",blueBall);
         ball2.addAnimation("blueRunning",blueBallImg);
         ball2.scale=0.03;
-        ball2.setCollider("circle",0,0,1000);
+        ball2.setCollider("circle",0,0,1000);*/
      }
+     
+     updatePosition(index)
+     {
+        var playerIndex = "players/player" + index;
+        database.ref(playerIndex).update({
+            xPos: this.ballSprite.x,
+            yPos:this.ballSprite.y
+        });
+     }
+
     }
